64a57feac9b3eafefc0bc5fb9ba6dad3d7edb6f8
Julio Nienow <julio.nienow@hotmail.com>
2017-05-20 14:50:05 -0300

    added scripts to dont save blank forms, added react effect on inputs, added slide effect mobile

--- js/main.js
+++ js/main.js
@@ -8,13 +8,15 @@ $(function() {
 	   $(".body_profile-settings > h2").html("SETTINGS");
 	}
 
+
+	//go to second website screen
 	$(".login_bg .login_box button").click(function() {
 
 		$(".login_bg").hide(500);
 		$(".website_bg").show(500);
 
 
-		//if user logedout and login again he will start with info window showing
+		//if user logedout and login again he will start with info window showing even if he left with settings openned
 		$(".body_profile-info").show(500);
 		$(".body_profile-settings").hide(500);
 		$(".header_bottom_nav a.about").addClass("selected");
@@ -22,6 +24,7 @@ $(function() {
 
 	})
 
+	//goes to login screen
 	$(".header_logout button").click(function() {
 
 		$(".login_bg").show(500);
@@ -29,6 +32,8 @@ $(function() {
 
 	})
 
+
+	//MOBILE hides about and shows settings
 	$(".website_bg .body_profile-info .editabsolute").click(function() {
 
 		$(".body_profile-info").hide();
@@ -36,6 +41,7 @@ $(function() {
 
 	})
 
+	//MOBILE hide settings and shows about 
 	$(".body_profile-info_edit .cancel").click(function() {
 
 		$(".body_profile-info_edit").hide();
@@ -44,10 +50,12 @@ $(function() {
 	})
 
 
+	//Changes the selected tab
 	$(".header_bottom_nav a.settings").click(function() { 
 
 			$(".body_profile-info").hide(500);
 			$(".body_profile-settings").show(500);
+			$(".body_profile-info_edit").hide();
 
 			$(".header_bottom_nav a.about").removeClass("selected");
 			$(".header_bottom_nav a.settings").addClass("selected");
@@ -55,60 +63,27 @@ $(function() {
 		// i would use foreach if tehre was more menu content for other links
 	})
 
+	//Changes the selected tab
 	$(".header_bottom_nav a.about, .body_profile-settings form .save").click(function() { 
 
 			$(".body_profile-info").show(500);
 			$(".body_profile-settings").hide(500);
+			$(".body_profile-info_edit").hide();
 
 			$(".header_bottom_nav a.about").addClass("selected");
 			$(".header_bottom_nav a.settings").removeClass("selected");
 
 	})
 
+	//MOBILE Changes the selected tab
 	$(".header_bottom_nav a.about, .body_profile-settings form .cancel").click(function() { 
 
 			$(".body_profile-info").show(500);
 			$(".body_profile-settings").hide(500);
+			$(".header_bottom_nav a.about").addClass("selected");
+			$(".header_bottom_nav a.settings").removeClass("selected");
 
 	})
-
-	//mobile edditing and saving scripts
-
-	$(".body_profile-info_edit .save").click(function() {
-
-		$(".body_profile-info_edit").hide();
-		$(".body_profile-info").show(500);
-
-		if ( $("#formfirst").val() ) {
-			var text1 = document.getElementById("formfirst").value; 
-			$(".body_profile-info h3 span.first").text(text1);
-		}
-
-		if ( $("#formlast").val() ) {
-			var text2 = document.getElementById("formlast").value; 
-			$(".body_profile-info h3 span.second").text(text2);
-		}
-
-		if ( $("#formsite").val() ) {
-			var text3 = document.getElementById("formsite").value; 
-			$(".body_profile-info h4 a").text(text3);
-			$(".body_profile-info h4 a").attr("href", text3)
-		}
-
-		if ( $("#formphone").val() ) {
-			var text4 = document.getElementById("formphone").value; 
-			$(".body_profile-info .ion-ios-telephone-outline h4").text(text4);
-
-		}
-
-		if ( $("#formadress").val() ) {
-			var text5 = document.getElementById("formadress").value; 
-			$(".body_profile-info .ion-ios-home-outline h4").text(text5);
-
-		}
-
-	})
-
 
 	//desktop about edditing and saving scripts
 
@@ -119,11 +94,22 @@ $(function() {
 
 		$(".body_profile-info .body_profile-info_loaded-name button.save").click(function() {
 
+
+
+			if ( $("#nameinfoedit").val() ) {
+
 				$(".nameeditdiv").hide(500);
 
 				var newname = document.getElementById("nameinfoedit").value;
+
 				$(".body_profile-info_loaded-name h3").text(newname);
 
+			}
+
+			else {
+
+				alert("Please fill this input");
+			}
 
 		})
 
@@ -144,12 +130,20 @@ $(function() {
 		$(".body_profile-info .body_profile-info_loaded-website button.save").click(function() {
 
 
+			if ( $("#websiteinfoedit").val() ) {
+
 				$(".websiteeditdiv").hide(500);
 
 				var newwebsite = document.getElementById("websiteinfoedit").value;
 				$(".body_profile-info_loaded-website h4 a").text(newwebsite);
 				$(".body_profile-info_loaded-website h4 a").attr("href", newwebsite)
 
+			}
+
+			else {
+
+				alert("Please fill this input");
+			}
 
 		})
 
@@ -169,11 +163,19 @@ $(function() {
 
 		$(".body_profile-info .body_profile-info_loaded-phone button.save").click(function() {
 
+			if ( $("#phoneinfoedit").val() ) {
+
 				$(".phoneeditdiv").hide(500);
 
 				var newphone = document.getElementById("phoneinfoedit").value;
 				$(".body_profile-info_loaded-phone h4").text(newphone);
 
+			}
+
+			else {
+
+				alert("Please fill this input");
+			}
 
 		})
 
@@ -192,11 +194,19 @@ $(function() {
 
 		$(".body_profile-info .body_profile-info_loaded-adress button.save").click(function() {
 
+			if ( $("#adressinfoedit").val() ) {
+
 				$(".adresseditdiv").hide(500);
 
 				var newadress = document.getElementById("adressinfoedit").value;
 				$(".body_profile-info_loaded-adress h4").text(newadress);
 
+			}
+
+			else {
+
+				alert("Please fill this input");
+			}
 
 		})
 
@@ -208,11 +218,103 @@ $(function() {
 
 	})
 
+	//adding css animate to login input
 
+	$(".login_box form #loginemail").click(function() {
+		var currentinput = $(this).attr('placeholder');
+		$(".login_box-email_input_text").text(currentinput);
+		$(this).attr('placeholder', '');
+	})
+
+	$(".login_box form #loginpass").click(function() {
+		var currentinput = $(this).attr('placeholder');
+		$(".login_box-password_input_text").text(currentinput);
+		$(this).attr('placeholder', '');
+	})
+
+	
+	//adding css animate to settings input
+
+	$(".body_profile-settings form #settingscurrent").click(function() {
+		var currentinput = $(this).attr('placeholder');
+		$(".body_profile-settings-settingscurrent").text(currentinput);
+		$(this).attr('placeholder', '');
+	})
+
+	$(".body_profile-settings form #settingsnew").click(function() {
+		var currentinput = $(this).attr('placeholder');
+		$(".body_profile-settings-settingsnew").text(currentinput);
+		$(this).attr('placeholder', '');
+	})
+
+	$(".body_profile-settings form #settingsrepeat").click(function() {
+		var currentinput = $(this).attr('placeholder');
+		$(".body_profile-settings-settingsrepeat").text(currentinput);
+		$(this).attr('placeholder', '');
+	})
 
 
 	
+	//adding css animate to mobile about edit
 
+	$(".body_profile-info_edit #formfirst").click(function() {
+		var currentinput = $(this).attr('placeholder');
+		$(".body_profile-info_edit-_firstmobile").text(currentinput);
+		$(this).attr('placeholder', '');
+	})
+
+	$(".body_profile-info_edit #formlast").click(function() {
+		var currentinput = $(this).attr('placeholder');
+		$(".body_profile-info_edit-_lastmobile").text(currentinput);
+		$(this).attr('placeholder', '');
+	})
+
+	$(".body_profile-info_edit #formsite").click(function() {
+		var currentinput = $(this).attr('placeholder');
+		$(".body_profile-info_edit-_websitemobile").text(currentinput);
+		$(this).attr('placeholder', '');
+	})
+
+	$(".body_profile-info_edit #formphone").click(function() {
+		var currentinput = $(this).attr('placeholder');
+		$(".body_profile-info_edit-_phonemobile").text(currentinput);
+		$(this).attr('placeholder', '');
+	})
+
+	$(".body_profile-info_edit #formadress").click(function() {
+		var currentinput = $(this).attr('placeholder');
+		$(".body_profile-info_edit-_adressmobile").text(currentinput);
+		$(this).attr('placeholder', '');
+	})
+
+
+	//add swipe left and right function
+	$(".body_profile-info").on("swipeleft",function(){
+		$(".body_profile-info").hide(500);
+		$(".body_profile-settings").show();
+
+		$(".header_bottom_nav a.about").removeClass("selected");
+		$(".header_bottom_nav a.settings").addClass("selected");
+	});
+
+	$(".body_profile-settings").on("swiperight",function(){
+		$(".body_profile-info").show(500);
+		$(".body_profile-settings").hide();
+
+
+		$(".header_bottom_nav a.about").addClass("selected");
+		$(".header_bottom_nav a.settings").removeClass("selected");
+	});
+
+	$(".body_profile-info_edit").on("swipeleft",function(){
+		$(".body_profile-info_edit").hide(500);
+		$(".body_profile-settings").show();
+
+		$(".header_bottom_nav a.about").removeClass("selected");
+		$(".header_bottom_nav a.settings").addClass("selected");
+	});
+
+	
 
 
 })
